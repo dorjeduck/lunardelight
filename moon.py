78c0caa09476e923b118a1f1be0879049d70e896
Martin Dudek <martin.dudek@gmail.com>
2023-10-16 12:43:43 +0200

    pos angle mod

--- moon.py
+++ moon.py
@@ -95,8 +95,11 @@ if posangle:
 nasa_png = os.path.join(__dir__,'nasa','moon.{:03d}.png'.format(nasa_approx["img_num"]))
 
 if moon_info['position_angle']:
-    output_name += "_{:.03f}_{:.03f}".format(*latlon)
-    
+    if latlon:
+        output_name += "_{:.03f}_{:.03f}".format(*latlon)
+    else:
+        output_name += "_" . round(moon_info['position_angle'])
+
     TAT.rotate_image(nasa_png,output_name + '.png',moon_info['position_angle'].degrees - 90 )
     if latlon:
         moon_info['latitude'],moon_info['longitude'] = latlon[0], latlon[1]
